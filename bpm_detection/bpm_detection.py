7313953b9ab89e1a7969b34840265d82ec62e1eb
david <david@newdelli.(none)>
2013-06-02 00:16:41 -0400

    Separated the algorithm into separate functions so that I could calculate the BPM for a single window instead of just a whole song/wav file.  This is allow for streaming in the future hopefully.

--- bpm_detection/bpm_detection.py
+++ bpm_detection/bpm_detection.py
@@ -35,26 +35,83 @@ def read_wav(filename):
     
     return samps, fs
     
+# simple peak detection
+def peak_detect(data):
+    max_val = numpy.amax(abs(data)) 
+    peak_ndx = numpy.where(data==max_val)
+    if len(peak_ndx[0]) == 0: #if nothing found then the max must be negative
+        peak_ndx = numpy.where(data==-max_val)
+    return peak_ndx
+    
+def bpm_detector(data):
+    cA = [] 
+    cD = []
+    correl = []
+    cD_sum = []
+    levels = 4
+    max_decimation = 2**(levels-1);
+    min_ndx = 60./ 220 * (fs/max_decimation)
+    max_ndx = 60./ 40 * (fs/max_decimation)
+    
+    for loop in range(0,levels):
+        cD = []
+        # 1) DWT
+        if loop == 0:
+            [cA,cD] = pywt.dwt(data,'db4');
+            cD_minlen = len(cD)/max_decimation+1;
+            cD_sum = numpy.zeros(cD_minlen);
+        else:
+            [cA,cD] = pywt.dwt(cA,'db4');
+        # 2) Filter
+        cD = signal.lfilter([0.01],[1 -0.99],cD);
+
+        # 4) Subtractargs.filename out the mean.
+
+        # 5) Decimate for reconstruction later.
+        cD = abs(cD[::(2**(levels-loop-1))]);
+        
+        cD = cD - numpy.mean(cD);
+        # 6) Recombine the signal before ACF
+        #    essentially, each level I concatenate 
+        #    the detail coefs (i.e. the HPF values)
+        #    to the beginning of the array
+        cD_sum = cD[0:cD_minlen] + cD_sum;
+
+    # adding in the approximate data as well...    
+    cA = signal.lfilter([0.01],[1 -0.99],cA);
+    cA = abs(cA);
+    cA = cA - numpy.mean(cA);
+    cD_sum = cA[0:cD_minlen] + cD_sum;
+    
+    # ACF
+    correl = numpy.correlate(cD_sum,cD_sum,'full') 
+    
+    midpoint = len(correl) / 2
+    correl_midpoint_tmp = correl[midpoint:]
+    peak_ndx = peak_detect(correl_midpoint_tmp[min_ndx:max_ndx]);
+    peak_ndx_adjusted = peak_ndx[0]+min_ndx;
+    bpm = 60./ peak_ndx_adjusted * (fs/max_decimation)
+    print bpm
+    return bpm,correl
+    
+    
     
 def bpm_detection(samps,fs,window=3):
     bpm = 0
-    levels = 4; 
     nsamps = len(samps)
-    window_samps = window*fs         
-    window_ndx = 1; #current window we are processing
+    window_samps = int(window*fs)         
+    window_ndx = int(1); #current window we are processing
     samps_ndx = 0;  #first sample in window_ndx 
-    accum_correl = []
     max_window_ndx = nsamps / window_samps;
 
     
+    data = []
+    correl=[]
+    bpms = numpy.zeros(max_window_ndx)
+
     #iterate through all windows
     while window_ndx < max_window_ndx:
-        sys.stdout.write('=');
-        cA = [] 
-        cD = []
-        data = []
-        correl = []
-        cD_sum = []
+
 
         #get a new set of samples
         data = samps[samps_ndx:samps_ndx+window_samps]
@@ -62,74 +119,36 @@ def bpm_detection(samps,fs,window=3):
             raise AssertionError( str(len(data) ) ) 
         #print 'Read', len(data), 'samples. From',str(samps_ndx+1),'to',str(samps_ndx+window_samps)
         
-        
-        #4 level loop
-        max_decimation = 2**(levels-1);
-        for loop in range(0,levels):
-            cD = []
-            # 1) DWT
-            if loop == 0:
-                [cA,cD] = pywt.dwt(data,'db4');
-                cD_minlen = len(cD)/max_decimation+1;
-                cD_sum = numpy.zeros(cD_minlen);
-            else:
-                [cA,cD] = pywt.dwt(cA,'db4');
-            # 2) Filter
-            cD = signal.lfilter([0.01],[1 -0.99],cD);
+        bpms[window_ndx],correl = bpm_detector(data)
 
-            # 4) Subtractargs.filename out the mean.
-
-            # 5) Decimate for reconstruction later.
-            cD = abs(cD[::(2**(levels-loop-1))]);
-            
-            cD = cD - numpy.mean(cD);
-            # 6) Recombine the signal before ACF
-            #    essentially, each level I concatenate 
-            #    the detail coefs (i.e. the HPF values)
-            #    to the beginning of the array
-            cD_sum = cD[0:cD_minlen] + cD_sum;
-
-        # adding in the approximate data as well...    
-        cA = signal.lfilter([0.01],[1 -0.99],cA);
-        cA = abs(cA);
-        cA = cA - numpy.mean(cA);
-        cD_sum = cA[0:cD_minlen] + cD_sum;
-        
-        # ACF
-        correl = numpy.correlate(cD_sum,cD_sum,'full')
         # integration...
-        if window_ndx == 1:
-            accum_correl = numpy.zeros(len(correl))
-        accum_correl = (correl+accum_correl) / len(correl) 
+        #if window_ndx == 1:
+        #    accum_correl = numpy.zeros(len(correl))
+        #accum_correl = (correl+accum_correl) / len(correl)
         
         #iterate at the end of the loop
         window_ndx = window_ndx + 1;
         samps_ndx = samps_ndx+window_samps;
 
-    sys.stdout.write('\n');
-
     # Peak detection - just involves only picking a peak within the window that we are 
-    accum_zero = len(correl) / 2
-    correl_final = accum_correl[accum_zero:]
+    #accum_zero = len(correl) / 2
+    #correl_final = accum_correl[accum_zero:]
     
-    min_ndx = 60./ 220 * (fs/max_decimation)
-    max_ndx = 60./ 40 * (fs/max_decimation)
-
-    val = numpy.amax(abs(correl_final[min_ndx:max_ndx]))
+    #val = numpy.amax(abs(correl_final[min_ndx:max_ndx]))    
+    #k = numpy.where(correl_final[min_ndx:max_ndx]==val)
+    #if len(k[0]) == 0:
+    #    k = numpy.where(correl_final[min_ndx:max_ndx]==-val)
+    #j = k[0]+min_ndx;
+    #bpm_final = 60./ j * (fs/max_decimation)
     
-    k = numpy.where(correl_final[min_ndx:max_ndx]==val)
-    if len(k[0]) == 0:
-        k = numpy.where(correl_final[min_ndx:max_ndx]==-val)
-    j = k[0]+min_ndx;
-    bpm = 60./ j * (fs/max_decimation)
-     
-    return bpm, correl_final
+    bpm = numpy.median(bpms)
+    return bpm, correl
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process .wav file to determine the Beats Per Minute.')
     parser.add_argument('--filename', required=True,
                    help='.wav file for processing')
-    parser.add_argument('--window', type=int, default=3,
+    parser.add_argument('--window', type=float, default=3,
                    help='size of the the window (seconds) that will be scanned to determine the bpm.  Typically less than 10 seconds. [3]')
 
     args = parser.parse_args()
